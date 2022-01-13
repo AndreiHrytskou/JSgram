bc1f796c6c2ef5d1f9fbd6e06818a16f81048925
Andrei Hrytskou <andreihrytskou@gmail.com>
2022-01-13 23:50:15 +0300

    done: form register and form singIn

--- js/home.js
+++ js/home.js
@@ -1,4 +1,6 @@
 function home() {
+  const links = document.querySelector("ul");
+  links.style.display = "flex";
   const main = document.createElement("div");
   document.body.append(main);
   main.className = "main";
